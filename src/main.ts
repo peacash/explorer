73606e951433fa152e00ddab711da3f7b1014fda
Lyserith <Lyserith@pm.me>
2022-10-15 19:36:00 +0200

    updates

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/index.css'
-import './styles/particles.css'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
