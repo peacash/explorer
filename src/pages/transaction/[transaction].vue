23e5d8a609b72091eb6dce6e0f25bf3c4dfa319f
Lyserith <Lyserith@pm.me>
2022-12-03 14:59:00 +0100

    updates

--- src/pages/transaction/[transaction].vue
+++ src/pages/transaction/[transaction].vue
@@ -1,3 +1,29 @@
+<style scoped>
+table {
+	font-family: 'Courier New', Courier, monospace;
+	font-weight: 600;
+	border-collapse: collapse;
+	width: 100%;
+}
+td {
+	border: 1px solid #ddd;
+	text-align: left;
+	padding: 8px;
+}
+td:nth-child(odd) {
+	padding-right: 16px;
+}
+td:nth-child(even) {
+	word-break: break-word;
+}
+tr:nth-child(even) {
+	background-color: #eee;
+}
+.link { text-decoration: none; }
+.link:hover {
+	text-decoration: underline;
+}
+</style>
 <template>
 	<div class="
 		mx-auto
@@ -10,8 +36,46 @@
 		md:max-w-7xl
 		grid md:grid-cols-2 gap-2 sm:gap-6
 	">
-		<Description v-if="transaction !== null" class="col-span-full text-justify md:text-left">
-			<Transaction :transaction="transaction" />
+		<Description class="col-span-full text-justify md:text-left">
+			<h2 class="mx-auto uppercase" style="font-weight: 300;">transaction</h2>
+			<table class="mx-auto">
+				<tr v-if="transaction">
+					<td>Hash</td>
+					<td v-if="shorten_hash" @click="(shorten_hash = false)">{{ shorten(transaction.hash) }}</td>
+					<td v-else>{{ transaction.hash }}</td>
+				</tr>
+				<tr v-if="transaction">
+					<td>Public&nbsp;key&nbsp;input</td>
+					<td v-if="shorten_public_key_input" @click="(shorten_public_key_input = false)">{{ shorten(transaction.public_key_input) }}</td>
+					<td v-else>
+        				<router-link class="link" :to="'/address/' + transaction.public_key_input">{{ transaction.public_key_input }}</router-link>
+					</td>
+				</tr>
+				<tr v-if="transaction">
+					<td>Public&nbsp;key&nbsp;output</td>
+					<td v-if="shorten_public_key_output" @click="(shorten_public_key_output = false)">{{ shorten(transaction.public_key_output) }}</td>
+					<td v-else>
+        				<router-link class="link" :to="'/address/' + transaction.public_key_output">{{ transaction.public_key_output }}</router-link>
+					</td>
+				</tr>
+				<tr v-if="transaction">
+					<td>Amount</td>
+					<td>{{ balance_to_string(transaction.amount) }}</td>
+				</tr>
+				<tr v-if="transaction">
+					<td>Fee</td>
+					<td>{{ balance_to_string(transaction.fee) }}</td>
+				</tr>
+				<tr v-if="transaction">
+					<td>Timestamp</td>
+					<td>{{ new Date(transaction.timestamp * 1000).toLocaleString() }}</td>
+				</tr>
+				<tr v-if="transaction">
+					<td>Signature</td>
+					<td v-if="shorten_signature" @click="(shorten_signature = false)">{{ shorten(transaction.signature) }}</td>
+					<td v-else>{{ transaction.signature }}</td>
+				</tr>
+			</table>
 			<div class="my-40"></div>
 		</Description>
 	</div>
@@ -20,7 +84,11 @@
 export default {
     data() {
 		return {
-			transaction: null
+			transaction: null,
+			shorten_hash: true,
+			shorten_public_key_input: true,
+			shorten_public_key_output: true,
+			shorten_signature: true
 		}
 	},
 	methods: {
@@ -29,6 +97,23 @@ export default {
 			fetch(window.localStorage.getItem("api") + "/transaction/" + this.$route.params.transaction).then(res => res.json()).then(data => {
 				this.transaction = data
 			})
+		},
+		shorten(string) {
+			return string.slice(0, 12) + "..." + string.slice(-8)
+		},
+		balance_to_string(balance) {
+			let decimals = 18
+			let string = '0'.repeat(decimals)
+			string += balance
+			let index = string.length - decimals
+			string = [string.slice(0, index), ".", string.slice(index)].join('')
+			while (string.startsWith("0")) {
+				string = string.slice(1)
+			}
+			if (string.startsWith(".")) {
+				string = "0" + string
+			}
+			return string
 		}
 	},
     mounted() {
