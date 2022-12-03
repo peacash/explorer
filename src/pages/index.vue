a54e69a579df9c2a14086591f29dfdb6f9da588e
Lyserith <Lyserith@pm.me>
2022-12-03 15:37:21 +0100

    updates

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,28 @@
 <style scoped>
-	* {
-		font-weight: 600;
-	}
+table {
+	font-family: 'Courier New', Courier, monospace;
+	font-weight: 600;
+	border-collapse: collapse;
+	width: 100%;
+}
+td {
+	border: 1px solid #ddd;
+	text-align: left;
+	padding: 8px;
+}
+td:nth-child(odd) {
+	padding-right: 16px;
+}
+td:nth-child(even) {
+	word-break: break-word;
+}
+tr:nth-child(even) {
+	background-color: #eee;
+}
+.link { text-decoration: none; font-weight: 600; }
+.link:hover {
+	text-decoration: underline;
+}
 </style>
 <template>
 	<div class="
@@ -21,8 +42,32 @@
 				<Unresponsive :api="api" />
 			</div>
     		<h2 v-if="(sync || info)" class="mx-auto uppercase" style="font-weight: 300;">general</h2>
-			<Sync v-if="sync" :sync="sync" />
-			<Info v-if="info" :info="info" />
+			<table class="mx-auto">
+				<tr v-if="sync">
+					<td>Synchronization</td>
+					<td>{{ sync.sync }}</td>
+				</tr>
+				<tr v-if="sync">
+					<td>Last block seen</td>
+					<td>{{ sync.last }}</td>
+				</tr>
+				<tr v-if="sync">
+					<td>Height</td>
+					<td>{{ sync.height }}</td>
+				</tr>
+				<tr v-if="info">
+					<td>Time</td>
+					<td>{{ info.time }}</td>
+				</tr>
+				<tr v-if="info">
+					<td>Uptime</td>
+					<td>{{ info.uptime }}</td>
+				</tr>
+				<tr v-if="info">
+					<td>Tick</td>
+					<td>{{ info.lag }} ms</td>
+				</tr>
+			</table>
 			<h2 v-if="dynamic" class="mx-auto uppercase" style="font-weight: 300;">stakers</h2>
 			<Stakers v-if="dynamic" :state="dynamic" />
 			<h2 v-if="(dynamic || trusted)" class="mx-auto uppercase" style="font-weight: 300;">blocks</h2>
