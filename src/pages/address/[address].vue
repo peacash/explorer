94baac7eee69d334725c441d97c1e726adfa6845
Lyserith <Lyserith@pm.me>
2022-12-03 15:05:58 +0100

    updates

--- src/pages/address/[address].vue
+++ src/pages/address/[address].vue
@@ -1,3 +1,31 @@
+
+<style scoped>
+table {
+	font-family: 'Courier New', Courier, monospace;
+	font-weight: 600;
+	border-collapse: collapse;
+	width: 100%;
+}
+td {
+	border: 1px solid #ddd;
+	text-align: left;
+	padding: 8px;
+}
+td:nth-child(odd) {
+	padding-right: 16px;
+}
+td:nth-child(even) {
+	word-break: break-word;
+}
+tr:nth-child(even) {
+	background-color: #eee;
+}
+.link { text-decoration: none; }
+.link:hover {
+	text-decoration: underline;
+}
+</style>
+
 <template>
 	<div class="
 		mx-auto
@@ -13,11 +41,21 @@
 		<Description v-if="balance !== null && balance_staked !== null" class="col-span-full text-justify md:text-left">
 			<Bar class="mt-2" />
 			<h2 class="mx-auto uppercase" style="font-weight: 300;">address</h2>
-			<h4 class="mx-auto uppercase pb-12" style="font-weight: 500;">{{ $route.params.address }}</h4>
-			<div class="flex justify-center mx-auto">
-				<Field name="Balance:" :value="balance_to_string(balance)" />
-				<Field name="Staked:&nbsp;" :value="balance_to_string(balance_staked)" />
-			</div>
+			<table class="mx-auto">
+				<tr v-if="(public_key !== null)">
+					<td>Public&nbsp;key</td>
+					<td v-if="shorten_public_key" @click="(shorten_public_key = false)">{{ shorten(public_key) }}</td>
+					<td v-else>{{ public_key }}</td>
+				</tr>
+				<tr v-if="(balance !== null)">
+					<td>Balance</td>
+					<td>{{ balance_to_string(balance) }}</td>
+				</tr>
+				<tr v-if="(balance_staked !== null)">
+					<td>Balance&nbsp;staked</td>
+					<td>{{ balance_to_string(balance_staked) }}</td>
+				</tr>
+			</table>
 			<div class="my-40"></div>
 		</Description>
 	</div>
@@ -27,7 +65,9 @@ export default {
     data() {
 		return {
             balance: null,
-            balance_staked: null
+            balance_staked: null,
+			public_key: this.$route.params.address,
+            shorten_public_key: true
 		}
 	},
     mounted() {
@@ -69,6 +109,9 @@ export default {
 				string = "0" + string
 			}
 			return string
+		},
+		shorten(string) {
+			return string.slice(0, 12) + "..." + string.slice(-8)
 		}
 	},
 	watch: {
