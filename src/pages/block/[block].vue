a8d39a90acd44658d05084ef94f897de3c327472
Lyserith <Lyserith@pm.me>
2022-12-03 13:48:05 +0100

    update

--- src/pages/block/[block].vue
+++ src/pages/block/[block].vue
@@ -10,16 +10,21 @@
 		md:max-w-7xl
 		grid md:grid-cols-2 gap-2 sm:gap-6
 	">
-		<Description v-if="block !== null" class="col-span-full text-justify md:text-left">
+		<Description class="col-span-full text-justify md:text-left">
 			<h2 class="mx-auto uppercase" style="font-weight: 300;">block</h2>
 			<h4 class="mx-auto uppercase pb-12" style="font-weight: 500;">{{ $route.params.block }}</h4>
-			<Field v-if="height" name="Height" :value="height" />
-			<Field :value="new Date(block.timestamp * 1000).toLocaleString()" />
-			<Field name="Forger address:" :value="block.public_key" :to="'/address/' + block.public_key" />
-			<Field name="Previous:" :value="block.previous_hash" :to="'/block/' + block.previous_hash" />
-			<Field name="Signature:" :value="block.signature" />
-			<Transactions :transactions="block.transactions" />
-			<Stakes :stakes="block.stakes" />
+			<div class="flex justify-center mx-auto">
+				<Field v-if="height" name="Height" :value="height" />
+				<Field v-if="block" name="Forged" :value="new Date(block.timestamp * 1000).toLocaleString()" />
+			</div>
+			<div class="flex flex-col justify-center mx-auto">
+				<Field v-if="block" name="Hash:" :value="shorten(block.hash)" />
+				<Field v-if="block" name="Previous hash:" :value="shorten(block.previous_hash)" :to="'/block/' + block.previous_hash" />
+				<Field v-if="block" name="Forger address:" :value="shorten(block.public_key)" :to="'/address/' + block.public_key" />
+				<Field v-if="block" name="Signature:" :value="shorten(block.signature)" />
+			</div>
+			<Transactions v-if="block" :transactions="block.transactions" />
+			<Stakes v-if="block" :stakes="block.stakes" />
 			<div class="my-40"></div>
 		</Description>
 	</div>
@@ -41,6 +46,9 @@ export default {
 			fetch(window.localStorage.getItem("api") + "/height/" + this.$route.params.block).then(res => res.json()).then(data => {
 				this.height = data
 			})
+		},
+		shorten(string) {
+			return string.slice(0, 12) + "..." + string.slice(-8)
 		}
 	},
     mounted() {
